ddc224cf9f67ed92f06debed3f6abc9becad89d3
student <student@192.168.56.1>
2017-05-23 17:49:50 +0200

    3.2.1 Scenariusz oraz EstimatedTimeArrivalSteps

---
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatedTimeArrivalSteps.java
@@ -0,0 +1,32 @@
+package edu.iis.mto.bdd.trains.cucumber.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pl.Gdy;
+import cucumber.api.java.pl.Wtedy;
+import cucumber.api.java.pl.Zakładając;
+
+public class EstimatedTimeArrivalSteps {
+	@Zakładając("^chcę się dostać z Parramatta do TownHall$")
+	public void chcę_się_dostać_z_Parramatta_do_TownHall() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Zakładając("^następny pociąg odjeżdża o (\\d+):(\\d+) na linii Western$")
+	public void następny_pociąg_odjeżdża_o_na_linii_Western(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Gdy("^zapytam o godzinę przyjazdu$")
+	public void zapytam_o_godzinę_przyjazdu() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (\\d+):(\\d+)$")
+	public void powinienem_uzyskać_następujący_szacowany_czas_przyjazdu(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
