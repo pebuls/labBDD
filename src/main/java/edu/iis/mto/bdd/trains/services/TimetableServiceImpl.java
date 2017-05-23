9178c7ddc76c0ad0cb43bf31603468929425ffe4
patrykxk <patrix215@gmail.com>
2017-05-23 18:29:09 +0200

    proba wykonania pierwszego scenariusza

---
+++ src/main/java/edu/iis/mto/bdd/trains/services/TimetableServiceImpl.java
@@ -0,0 +1,87 @@
+package edu.iis.mto.bdd.trains.services;
+
+import com.google.common.collect.Lists;
+import edu.iis.mto.bdd.trains.model.Line;
+import org.joda.time.LocalTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimetableServiceImpl implements TimetableService {
+
+    public TimetableServiceImpl(List<Line> lines, List<LocalTime> departureTimes) {
+        this.lines = lines;
+        this.departureTimes = departureTimes;
+    }
+
+    private List<Line> lines;
+    // All trains leave the depots at the same time.
+    private List<LocalTime> departureTimes;
+
+    public List<Line> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<LocalTime> getDepartureTimes() {
+        return departureTimes;
+    }
+
+    public void setDepartureTimes(List<LocalTime> departureTimes) {
+        this.departureTimes = departureTimes;
+    }
+
+    @Override
+    public List<LocalTime> findArrivalTimes(Line line, String targetStation) {
+        Line targetLine = lineMatching(line);
+        int timeTaken = 0;
+        for (String station : targetLine.getStations()) {
+            if (station.equals(targetStation)) {
+                break;
+            }
+            timeTaken += 5;
+        }
+        List<LocalTime> arrivalTimes = Lists.newArrayList();
+        for (LocalTime time : departureTimes) {
+            arrivalTimes.add(time.plusMinutes(timeTaken));
+        }
+        return arrivalTimes;
+    }
+
+    private Line lineMatching(Line requestedLine) {
+        for (Line line : lines) {
+            if (line.equals(requestedLine)) {
+                return line;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Line> findLinesThrough(String departure, String destination) {
+        // return ImmutableList.of(lines.get(0));
+        List<Line> resultLines = new ArrayList<>();
+        for (Line line : lines) {
+            if (line.getStations().contains(departure) && line.getStations().contains(destination)) {
+                if (line.getStations().indexOf(destination) > line.getStations().indexOf(departure)) {
+                    resultLines.add(line);
+                }
+            }
+        }
+        return resultLines;
+    }
+
+    @Override
+    public void scheduleArrivalTime(String line, LocalTime departureTime) {
+        // To change body of created methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public LocalTime getArrivalTime(String travellingOnLine, String destination) {
+        // TODO: Call the back-end service to retrieve this data
+        return null;
+    }
+}
--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,10 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.TimetableService;
+import edu.iis.mto.bdd.trains.services.TimetableServiceImpl;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -11,18 +15,27 @@ import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
 public class OptimalItinerarySteps {
+    TimetableServiceImpl timetableService;
+    List<Line> lines;
+    Line lineObject;
 
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-        throw new PendingException();
 
+        List<LocalTime> givenDepartureTimes;
+        lineObject = Line.named(line).departingFrom(lineStart).withStations(departure, destination);
+        givenDepartureTimes = departureTimes;
+        List<Line> lines = ImmutableList.of(lineObject);
+
+        timetableService = new TimetableServiceImpl(lines, givenDepartureTimes);
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-        throw new PendingException();
+        List<LocalTime> times = timetableService.findArrivalTimes(lineObject, destination);
+        System.out.println("Czas: " + times);
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
