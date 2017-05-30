a1880930f91aa93519ed7c4f1dc35f9597e82549
patrykxk <patrix215@gmail.com>
2017-05-30 11:24:53 +0200

    1.2 refaktoring - zmiana nazwy metod i dodanie konwertera dla czasu

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimateTimeTravelSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimateTimeTravelSteps.java
@@ -1,23 +1,25 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.I;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import org.joda.time.LocalTime;
 
 /**
  * Created by Patryk on 2017-05-30.
  */
 public class EstimateTimeTravelSteps {
         @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"$")
-        public void givenFromTo(String from, String to) {
+        public void givenFromTo(String fromCity, String toCity) {
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
         }
 
     @I("^następny pociąg odjeżdża o \"([^\"]*)\" na linii \"([^\"]*)\"$")
-    public void nextTrainIsLiving(String arg0, String arg1) throws Throwable {
+    public void nextTrainIsLiving(@Transform(JodaLocalTimeConverter.class) LocalTime departingTime, String line) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
@@ -29,7 +31,7 @@ public class EstimateTimeTravelSteps {
     }
 
     @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: \"([^\"]*)\"$")
-    public void shouldBeInformedAboutTimeTravel(String time) throws Throwable {
+    public void shouldBeInformedAboutTimeTravel(@Transform(JodaLocalTimeConverter.class) LocalTime expectedArrivalTime) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
