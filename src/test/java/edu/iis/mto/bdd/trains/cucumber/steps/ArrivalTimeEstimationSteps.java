8d690d19c5f7003645f076b68e218e170b522be3
Konrad Gos <goskonrad@outlook.com>
2017-05-30 14:41:49 +0200

    [feature2_rev1] + scenario template and automation

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeEstimationSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeEstimationSteps.java
@@ -1,35 +1,34 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
+import cucumber.api.java.pl.I;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import org.joda.time.LocalTime;
 
 /**
  * Created by Konrad Gos on 30.05.2017.
  */
 public class ArrivalTimeEstimationSteps {
-    @Zakładając("^chcę się dostać z Parramatta do TownHall$")
-    public void chcę_się_dostać_z_Parramatta_do_TownHall() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"")
+    public void chce_się_dostać_z_do(String startStation, String stopStation) {
         throw new PendingException();
     }
 
-    @Zakładając("^następny pociąg odjeżdża o (\\d+):(\\d+) na linii Western$")
-    public void następny_pociąg_odjeżdża_o_na_linii_Western(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @I("^następny pociąg odjeżdża o (.*) na linii \"(.*)\"")
+    public void następny_pociąg_odjeżdża_o_na_linii(@Transform(JodaLocalTimeConverter.class) LocalTime departureTime, String line) {
         throw new PendingException();
     }
 
-    @Gdy("^zapytam o godzinę przyjazdu$")
-    public void zapytam_o_godzinę_przyjazdu() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @Gdy("^zapytam o godzinę przyjazdu")
+    public void zapytam_o_godzinę_przyjazdu() {
         throw new PendingException();
     }
 
-    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (\\d+):(\\d+)$")
-    public void powinienem_uzyskać_następujący_szacowany_czas_przyjazdu(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu:(.*)$")
+    public void powinienem_uzyskać_natępujący_czas_przyjazdu(@Transform(JodaLocalTimeConverter.class) LocalTime estimationArrivalTime) {
         throw new PendingException();
     }
 }
