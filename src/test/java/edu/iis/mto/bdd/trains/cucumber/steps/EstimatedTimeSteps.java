1300c8957e7783dfc37bfa9292fb08316e2a0817
Khandree <a.lis.marciniak@gmail.com>
2017-06-20 00:48:05 +0200

    feature2_rev1 Scenario changed to scenario template

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatedTimeSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatedTimeSteps.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
+import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.I;
@@ -13,21 +14,21 @@ import org.joda.time.LocalTime;
 public class EstimatedTimeSteps {
     @Zakładając("^chcę się dostać z \"(.*)\" do \"(.*)\"$")
     public void given(String from, String to) {
-
+        throw new PendingException();
     }
 
     @I("^następny pociąg odjeżdża o \"(.*)\" z linii \"(.*)\"$")
     public void and(@Transform(JodaLocalTimeConverter.class) LocalTime departureTime, String startStation) {
-
+        throw new PendingException();
     }
 
     @Gdy("^zapytam o godzinę przyjazdu$")
     public void when() {
-
+        throw new PendingException();
     }
 
     @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: \"(.*)\"$")
     public void then(@Transform(JodaLocalTimeConverter.class) LocalTime arriveTime) {
-
+        throw new PendingException();
     }
 }
